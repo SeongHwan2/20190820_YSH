3118afa2454367673b681905cabc4651d59ba62a
GD7 <GD7@DESKTOP-58VHOFH>
2019-08-20 15:49:38 +0900

    test7

--- src/main/java/com/java/Home.java
+++ src/main/java/com/java/Home.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.java.hdfs.Hadoop;
 
-@WebServlet("/hdfs-test/Home")
+@WebServlet("/20190820_YSH/Home")
 public class Home extends HttpServlet {
 	
 	// 시작 화면 출력
